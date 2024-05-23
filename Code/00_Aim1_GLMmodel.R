9d220bdc50d5493e124fcf488860385e0ead50a7
Xucheng (Fred) Huang <123673440+Fredatlhhh@users.noreply.github.com>
2024-05-23 01:15:24 -0400

    Update 00_Aim1_GLMmodel.R

--- Code/00_Aim1_GLMmodel.R
+++ Code/00_Aim1_GLMmodel.R
@@ -37,7 +37,6 @@ source("Code/0702_SingleSessionARIMAWorkflow.R")
 
 ############################################################################################################################################################
 ################### Data download: ET data; fMRI data; head motion data ###################
-options(timeout = 6000) # Large data files take more time to download, please increase the time in case of poor network conditions
 
 # Define the Dropbox URL
 url <- "https://www.dropbox.com/scl/fo/yvxr7s8ivy9vei4plozgr/ADvFl4wxAiFx367d7vZzUWo?rlkey=50c1mrlvjftkdl9ifcp5c7thw&st=t9s5mx4h&dl=1"
