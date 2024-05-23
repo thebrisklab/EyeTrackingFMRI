f66be06634b6be8565c9a8b89d77738efc391f6b
Xucheng (Fred) Huang <123673440+Fredatlhhh@users.noreply.github.com>
2024-05-23 01:20:13 -0400

    Update 01_Subject_fMRIRegionMeanTimeSeries.R

--- Code/Small_Functions/01_Subject_fMRIRegionMeanTimeSeries.R
+++ Code/Small_Functions/01_Subject_fMRIRegionMeanTimeSeries.R
@@ -10,38 +10,35 @@
 # Output:
 # - A matrix with brain regions as rows and time points as columns (region by time).
 
-fMRI_xii_pmean <- function(dtseries_data, brain_parcellation = "Schaefer_100", region_count = 100) {
-  # Load the brain parcellation data
+fMRIBrain_Mapping <- function(dtseries_data, mapping_data, brain_parcellation = "Schaefer_100", region_count = 100) {
+  # Load the parcellation data
   parc <- load_parc(brain_parcellation)
   
-  # Convert parcellation data into a vector, indicating the associated brain region
+  # Convert the parcellation to a vector to identify the brain region of each voxel
   parc_vec <- c(as.matrix(parc))
   
-  # Adjust for non-cortical vertices to align with the parcellation scheme
+  # Adjust subcortex labels to match cortical labeling
+  subcortex_labels_adjusted <- as.numeric(dtseries_data$meta$subcort$labels) - 2
+  subcortex_labels_adjusted <- region_count + subcortex_labels_adjusted
+  
+  # Create a combined brain vector of cortical and adjusted subcortex labels
+  brain_vec <- c(parc_vec, subcortex_labels_adjusted)
+  
+  # Adjust the fMRI data for non-cortical vertices to align with parcellation
   adjusted_data <- move_from_mwall(dtseries_data, NA)
   
-  # Convert the adjusted data into a matrix format (voxel by time)
-  xii_mat <- as.matrix(adjusted_data)
+  # Initialize fMRI data with empty values for subsequent mapping
+  prepared_data <- select_xifti(adjusted_data * 0, 1)
   
-  # Retrieve and process the labels for subcortex regions
-  subcortex_labels <- adjusted_data$meta$subcort$labels
+  # If the data represents p-values, perform -log10 transformation for visualization
+  # Uncomment the following line if necessary
+  # transformed_data <- -log10(mapping_data)
   
-  # Combine cortical and subcortex labels
-  combined_labels <- c(as.character(parc_vec), as.character(subcortex_labels))
+  # Map the statistical data onto the brain vector
+  brain_graph <- newdata_xifti(prepared_data, c(NA, mapping_data)[brain_vec + 1])
   
-  # Generate labels for brain regions including cortex and subcortex
-  labels_vector <- c(as.character(1:region_count), levels(subcortex_labels)[3:21])
-  
-  # Initialize a matrix to store the computed mean fMRI signal values
-  regional_means <- matrix(nrow = region_count + 19, ncol = ncol(xii_mat))
-  
-  # Calculate mean signals for each parcellation region across time points
-  for (label in labels_vector) {
-    region_data <- xii_mat[combined_labels == label, ]
-    regional_means[which(labels_vector == label), ] <- colMeans(region_data, na.rm = TRUE)
-  }
-  
-  return(regional_means)
+  # Return the brain graph object for visualization
+  return(brain_graph)
 }
 
 # Example of usage:
